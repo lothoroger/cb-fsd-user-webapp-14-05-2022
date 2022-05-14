329288bbd82f60526471debd07d4e686b2daa5aa
First Last <rlotholanl@ip-172-31-19-246.ec2.internal>
2022-05-14 15:42:31 +0000

    added User-Webapp project  setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ChangePasswordComponent } from './auth/change-password/change-password.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
+import { InvoiceComponent } from './components/pages/invoice/invoice.component';
+import { WishlistsComponent } from './components/pages/wishlists/wishlists.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ViewComponent } from './components/products/view/view.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { HomeComponent } from './components/home/home.component';
+import { SummaryPageComponent } from './components/pages/summary-page/summary-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChangePasswordComponent,
+    ProfileComponent,
+    InvoiceComponent,
+    WishlistsComponent,
+    ProductsComponent,
+    ViewComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    HomeComponent,
+    SummaryPageComponent
   ],
   imports: [
     BrowserModule,
